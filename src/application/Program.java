30fc22aa9dd6c17cb8f06cd97d5af8691da42526
Diogenes <diogenes.junior@gmail.com>
2021-06-06 11:32:26 -0300

    Duck class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import model.entities.Duck;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Duck duck = new Duck();
+		
+		duck.quack();
+		duck.swim();
+		duck.display();
 
 	}
 
---
+++ src/model/entities/Duck.java
@@ -0,0 +1,16 @@
+package model.entities;
+
+public class Duck {
+
+	public void quack() {
+		System.out.println("quack");
+	}
+	
+	public void swim() {
+		System.out.println("swim");
+	}
+	
+	public void display() {
+		System.out.println("display duck");
+	}
+}
