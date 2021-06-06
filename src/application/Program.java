e3ff34047374a6387931f68a6a04cfc9093b8f90
Diogenes <diogenes.junior@gmail.com>
2021-06-06 11:37:23 -0300

    MallardDuck class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,21 @@
 package application;
 
 import model.entities.Duck;
+import model.entities.MallardDuck;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Duck duck = new Duck();
+		Duck duck2 = new MallardDuck();
 		
 		duck.quack();
 		duck.swim();
 		duck.display();
+		
+		duck2.quack();
+		duck2.swim();
+		duck2.display();
 
 	}
 
